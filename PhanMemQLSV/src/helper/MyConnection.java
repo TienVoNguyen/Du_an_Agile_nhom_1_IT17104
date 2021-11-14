f04b8ba68e5b0dfc662792d05a82894e11bd31bf
Đồng Thái Dương <93709370+ShowMakerCode@users.noreply.github.com>
2021-11-15 03:15:59 +0700

    Update MyConnection.java

--- PhanMemQLSV/src/helper/MyConnection.java
+++ PhanMemQLSV/src/helper/MyConnection.java
@@ -10,7 +10,22 @@ package helper;
  * @author NTV
  */
 public class MyConnection {
-    
-    
+     public void ConnectionSQL(String SeverName_SQL) { 
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setUser("sa");
+        ds.setPassword("123");
+        ds.setPortNumber(1433);
+        ds.setServerName(SeverName_SQL);
+        ds.setDatabaseName("FPT_APP");
+        try {
+            Connection con = ds.getConnection();
+            con.close();
+        } catch (SQLServerException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
     
 }
