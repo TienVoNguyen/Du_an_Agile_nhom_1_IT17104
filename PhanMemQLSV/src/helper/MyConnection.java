e5eff8824c8a10dcd9662ad8d070c0b8f8c21251
DuangGoodBoy <98taeyang@gmail.com>
2021-11-15 03:05:46 +0700

    SQL_Connection

--- PhanMemQLSV/src/helper/MyConnection.java
+++ PhanMemQLSV/src/helper/MyConnection.java
@@ -5,10 +5,31 @@
  */
 package helper;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author NTV
  */
 public class MyConnection {
-    
+           public void ConnectionSQL(String SeverName_SQL) {
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setUser("sa");
+        ds.setPassword("123");
+        ds.setPortNumber(1433);
+        ds.setServerName(SeverName_SQL);
+        ds.setDatabaseName("FPT_APP");
+        try {
+            Connection con = ds.getConnection();
+            con.close();
+        } catch (SQLServerException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
 }
