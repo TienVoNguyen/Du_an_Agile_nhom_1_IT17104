892d423e10bceeab4bd803aac6df9de3889843ba
Đồng Thái Dương <93709370+ShowMakerCode@users.noreply.github.com>
2021-11-15 15:09:43 +0700

    Update MyConnection.java

--- PhanMemQLSV/src/helper/MyConnection.java
+++ PhanMemQLSV/src/helper/MyConnection.java
@@ -4,28 +4,11 @@
  * and open the template in the editor.
  */
 package helper;
-
 /**
  *
  * @author NTV
  */
 public class MyConnection {
-     public void ConnectionSQL(String SeverName_SQL) { 
-        SQLServerDataSource ds = new SQLServerDataSource();
-        ds.setUser("sa");
-        ds.setPassword("123");
-        ds.setPortNumber(1433);
-        ds.setServerName(SeverName_SQL);
-        ds.setDatabaseName("FPT_APP");
-        try {
-            Connection con = ds.getConnection();
-            con.close();
-        } catch (SQLServerException ex) {
-            ex.printStackTrace();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
-    }
+  
     
 }
