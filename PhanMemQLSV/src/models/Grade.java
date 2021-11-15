13cfdacd9db1ff61449fcbc531fadbb60f1989b6
maihuong <huongntph18586@fpt.edu.vn>
2021-11-15 16:52:08 +0700

    models

--- PhanMemQLSV/src/models/Grade.java
+++ PhanMemQLSV/src/models/Grade.java
@@ -10,5 +10,48 @@ package models;
  * @author NTV
  */
 public class Grade {
-    
+    private int id;
+    private float tiengAnh,tinHoc,gDTC;
+
+    public Grade() {
+    }
+
+    public Grade(int id, float tiengAnh, float tinHoc, float gDTC) {
+        this.id = id;
+        this.tiengAnh = tiengAnh;
+        this.tinHoc = tinHoc;
+        this.gDTC = gDTC;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getTiengAnh() {
+        return tiengAnh;
+    }
+
+    public void setTiengAnh(float tiengAnh) {
+        this.tiengAnh = tiengAnh;
+    }
+
+    public float getTinHoc() {
+        return tinHoc;
+    }
+
+    public void setTinHoc(float tinHoc) {
+        this.tinHoc = tinHoc;
+    }
+
+    public float getgDTC() {
+        return gDTC;
+    }
+
+    public void setgDTC(float gDTC) {
+        this.gDTC = gDTC;
+    }
 }
