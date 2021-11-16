889e5e1533518015b54f64d940e28f4f303e4bca
Tae Yang <98taeyang@gmail.com>
2021-11-16 22:15:59 +0700

    Validate

--- PhanMemQLSV/src/helper/MyValidate.java
+++ PhanMemQLSV/src/helper/MyValidate.java
@@ -49,61 +49,62 @@ public class MyValidate {
         return false;
     }
 
-    public static boolean isNotNumberScores(JTextField txtValue, String msg_Format ,String msg_Domain) {
+    public static boolean isNotNumberScores(JTextField txtValue, String msg_Format, String msg_Domain) {
         String Number = String.valueOf(txtValue.getText());
         String pattern = "[0-9]{1,2}";
-        if (Number.matches(pattern) == false ) {
-          MyMessage.msgWarning(msg_Format);
-          txtValue.setBackground(Color.YELLOW);
-          txtValue.requestFocus();
+        if (Number.matches(pattern) == false) {
+            MyMessage.msgWarning(msg_Format);
+            txtValue.setBackground(Color.YELLOW);
+            txtValue.requestFocus();
             return true;
         }
-        if (Double.parseDouble(Number)<0 | Double.parseDouble(Number)>10) {
-          MyMessage.msgWarning(msg_Domain);
-          txtValue.setBackground(Color.YELLOW);
-          txtValue.requestFocus();
+        if (Double.parseDouble(Number) < 0 | Double.parseDouble(Number) > 10) {
+            MyMessage.msgWarning(msg_Domain);
+            txtValue.setBackground(Color.YELLOW);
+            txtValue.requestFocus();
             return true;
-        } 
-        
+        }
+
         return false;
 
     }
-    public static boolean isNotNumberPhone(JTextField txtValue, String msg){
-        String Number =  String.valueOf(txtValue.getText());
+
+    public static boolean isNotNumberPhone(JTextField txtValue, String msg) {
+        String Number = String.valueOf(txtValue.getText());
         String pattern = "[0-9]{8,15}";
-       if (Number.matches(pattern) == false ) {
-          MyMessage.msgWarning(msg);
-          txtValue.setBackground(Color.YELLOW);
-          txtValue.requestFocus();
+        if (Number.matches(pattern) == false) {
+            MyMessage.msgWarning(msg);
+            txtValue.setBackground(Color.YELLOW);
+            txtValue.requestFocus();
             return true;
         }
-    
-          return false;
-    }
-     public static boolean isNotEmail(JTextField txtValue, String msg){
-     String Gmail =  String.valueOf(txtValue.getText());
-     String pattern = "^[a-zA-Z]\\w{2,}@\\w{2,}(\\.\\w{2,3}){1,2}$";
-         if (Gmail.matches(pattern)==false) {
-          MyMessage.msgWarning(msg);
-          txtValue.setBackground(Color.YELLOW);
-          txtValue.requestFocus();
-          return true;   
-         }
-     
-     
-     return false;
-     }
-    
-     public static boolean isNotStudenCode(JTextField txtValue, String msg){
-       String Number =  String.valueOf(txtValue);
-         String pattern = "PH[0-9]{2,10}";
-         if (Number.matches(pattern) == false) {
-           MyMessage.msgWarning(msg);
-          txtValue.setBackground(Color.YELLOW);
-          txtValue.requestFocus();
-             return true;
-        }
-        
+
         return false;
-     }
+    }
+
+    public static boolean isNotEmail(JTextField txtValue, String msg) {
+        String Gmail = String.valueOf(txtValue.getText());
+        String pattern = "^[a-zA-Z]\\w{2,}@\\w{2,}(\\.\\w{2,3}){1,2}$";
+        if (Gmail.matches(pattern) == false) {
+            MyMessage.msgWarning(msg);
+            txtValue.setBackground(Color.YELLOW);
+            txtValue.requestFocus();
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isNotStudenCode(JTextField txtValue, String msg) {
+        String Number = String.valueOf(txtValue);
+        String pattern = "PH[0-9]{2,10}";
+        if (Number.matches(pattern) == false) {
+            MyMessage.msgWarning(msg);
+            txtValue.setBackground(Color.YELLOW);
+            txtValue.requestFocus();
+            return true;
+        }
+
+        return false;
+    }
 }
