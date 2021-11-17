3830dee77f9bd88229e9b225bee6ba7d540adb66
vontph18623 <vontph18623@fpt.edu.vn>
2021-11-17 08:29:29 +0700

    them check login

--- PhanMemQLSV/src/daos/UserDAO.java
+++ PhanMemQLSV/src/daos/UserDAO.java
@@ -5,7 +5,12 @@
  */
 package daos;
 
+import helper.MyConnection;
 import interfaces.UserInterface;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import models.User;
 
@@ -13,7 +18,7 @@ import models.User;
  *
  * @author NTV
  */
-public class UserDAO implements UserInterface<User>{
+public class UserDAO implements UserInterface<User> {
 
     @Override
     public ArrayList<User> getList() throws Exception {
@@ -21,8 +26,25 @@ public class UserDAO implements UserInterface<User>{
     }
 
     @Override
-    public boolean checkLogin(String username, String pass) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public User checkLogin(String username, String pass) throws Exception {
+        String sql = "SELECT * FROM [User] WHERE [username] = ? AND [password] = ?";
+        Connection con = MyConnection.ConnectionSQL();
+        try (
+                PreparedStatement pstm = con.prepareStatement(sql);) {
+            pstm.setString(1, username);
+            pstm.setString(2, pass);
+            try (
+                    ResultSet rs = pstm.executeQuery();) {
+                    if (rs.next()) {
+                        User user = new User();
+                        user.setUsername(username);
+                        user.setRole(rs.getString(3));
+                        return user;
+                    }
+            }
+        }
+
+        return null;
     }
-    
+
 }
